3d70e911967809ebed3262572baef2515772b246
erikaosterbur <erikaosterbur@gmail.com>
2021-09-19 12:55:35 -0500

    Adding variables, functions, and conditional statements

--- script.js
+++ script.js
@@ -1,68 +1,82 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var numberOfCharacters = window.prompt ("How many characters do you want in your password?");
+var numberOfCharacters = window.prompt ("How many characters do you want in your password? (8-128)");
 var specialCharsAllowed = window.confirm ("Do you want special characters in your password?");
 var numbersAllowed = window.confirm ("Do you want numbers in your password?");
 var lowercaseAllowed = window.confirm ("Do you want lowercase letters in your password?");
 var uppercaseAllowed = window.confirm ("Do you want uppercase letters in your password?");
 
-//How many characters password will have
-//Are special characters included?
-//Are numbers included?
-//Are lowercase letters included?
-//Are uppercase letters included?
 
-//Build final list of possible characters
-//How can we build our password one letter at a time?
-
-//For each character needed, grab a random element from the allowed list
-
-
-
-// function askNumberOfCharacters(){
-
-// }
 var numberOfCharacters = parseInt(numberOfCharacters);
 
-if (specialCharsAllowed){
-  var specialCharsAllowed = ["!", "#", "$", "%", "&", "(", ")", "*", ",", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "|", "}", "~"];
+console.log (numberOfCharacters);
+
+function getSpecChar () {
+  if (specialCharsAllowed) {
+    var symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  return symbols
 }
-else specialCharsAllowed = null;
+};
 
-if (numbersAllowed){
-  var numbersAllowed = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-}
-else numbersAllowed = null;
+function getNumbers () {
+  if (numbersAllowed) {
+    var numbers = "0123456789"
+    }
+  return numbers
+  };
 
-if (lowercaseAllowed){
-  var lowercaseAllowed = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-}
-else lowercaseAllowed = null;
+function getLowercase () {
+  if (lowercaseAllowed) {
+    var lowercase = "abcdefghijklmnopqrstuvwxyz"
+  }
+  return lowercase
+};
 
-if (uppercaseAllowed) {
-  var uppercaseAllowed = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-}
-else uppercaseAllowed = null;
+function getUppercase () {
+  if (uppercaseAllowed) {
+    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  }
+  return uppercase
+};
 
-var characterSet = specialCharsAllowed + numbersAllowed + lowercaseAllowed + uppercaseAllowed;
+var characterSet = symbols.concat(numbers, lowercase, uppercase);
 
-console.log (characterSet);
+console.log(characterSet);
+
+function finalPassword () {
+  var password = [Math.floor(Math.random() * characterSet.length)];
+};
+
+console.log (password);
 
 
-function generatePassword(){
-  var finalpassword = getRandomValue(characterSet);
-  return finalpassword;
-}
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
-}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
+// var characterSet = function.concat(getSpecChar, getNumber, getLowercase, getUppercase);
+
+// console.log (characterSet);
+
+
+
+// function generatePassword(){
+//   var finalPassword = characterSet[Math.floor(Math.random() * numberOfCharacters)]
+//   return finalPassword;
+// }
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
