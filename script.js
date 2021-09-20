59f5625237066d35bacffcd54153b38016acbf12
erikaosterbur <erikaosterbur@gmail.com>
2021-09-20 16:35:40 -0500

    Added descriptive comments

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Start of function to generate password
 function generatePassword() {
   var numberOfCharacters = window.prompt ("How many characters do you want in your password? (between 8-128)");
   var specialCharsAllowed = window.confirm ("Do you want special characters in your password?");
@@ -8,15 +9,18 @@ function generatePassword() {
   var lowercaseAllowed = window.confirm ("Do you want lowercase letters in your password?");
   var uppercaseAllowed = window.confirm ("Do you want uppercase letters in your password?");
 
+  // Creates condition for if user chooses a number outside of the set parameters
   while (numberOfCharacters <= 8 || numberOfCharacters >= 128) {
     alert ("Choose a number between 8 and 128");
       numberOfCharacters = window.prompt ("How many characters do you want in your password? (between 8-128")
   };
 
+  // Turns string into number
   numberOfCharacters = parseInt(numberOfCharacters);
 
   console.log (numberOfCharacters);
 
+  // Functions that get different characters based on user selection
   function getSpecChar () {
     var symbols = "";
     if (specialCharsAllowed) {
@@ -49,15 +53,17 @@ function generatePassword() {
     return uppercase;
   }
 
+  // Combines all characters into one string
   var characterSet = getSpecChar () + getNumbers () + getLowercase () + getUppercase ()
   
   console.log (characterSet);
 
+  // Splits string to form new array
   var splitCharacters = characterSet.split("");
 
   console.log (splitCharacters);
 
-
+  // Takes all characters and generates random set with user-selected number of characters
   var result = "";
   for ( var i = 0; i < numberOfCharacters; i++ ) {
     result += characterSet.charAt(Math.floor(Math.random() * 
